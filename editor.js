3a047e7ebf81cc7fd8d05746ec89a05a23c56122
Manny Perez <threedyou@gmail.com>
2017-03-15 20:02:43 -0500

    Modified editor.html along with editor.js for enter key function listening on text   box with search function on Enter key events

--- editor.js
+++ editor.js
@@ -18,6 +18,30 @@ function handleHTTP(req,res) {
 }
 
 
+function connection(socket) {
+
+	function disconnect() {
+		console.log("disconnected");
+	}
+
+	function getmsg(msg) {
+		io.sockets.emit("broadcast",msg);
+	}
+
+	function spy(move) {
+		socket.broadcast.emit("spy",move);
+	}
+
+	socket.on("disconnect",disconnect);
+	socket.on("msg",getmsg);
+	socket.on("spy",spy);
+
+	var intv = setInterval(function(){
+		socket.emit("hello",Math.random());
+	},1000);
+}
+
+
 var
 	http = require("http"),
 	httpserv = http.createServer(handleHTTP),
@@ -27,10 +51,13 @@ var
 
 	ASQ = require("asynquence"),
 	node_static = require("node-static"),
+	static_files = new node_static.Server(__dirname),
+
 	io = require("socket.io").listen(httpserv)
 ;
 
 require("asynquence-contrib");
+
 
 // configure socket.io
 io.on("configure",function(){
@@ -44,20 +71,61 @@ io.on("configure",function(){
 	]);
 });
 
+
 httpserv.listen(port, host);
 
+io.on("connection",connection);
 
-io.on('connection', function(socket) {
 
 
-    socket.on('disconnect', function() {
-        console.log('Disconnected.');
-    });
-
-		function getDevice(dev) {
-		io.sockets.emit("broadcast",dev);
-	}
-
-	socket.on("dev",getDevice);
-
-});
+// io.on('connection', function(socket) {
+//
+// 	function disconnect() {
+// 		console.log("disconnected");
+// 	}
+//
+// 	function getmsg(msg) {
+// 		io.sockets.emit("broadcast",msg);
+// 	}
+//
+// 	function spy(move) {
+// 		socket.broadcast.emit("spy",move);
+// 	}
+//
+// 	socket.on("disconnect",disconnect);
+// 	socket.on("msg",getmsg);
+// 	socket.on("spy",spy);
+//
+// 	var intv = setInterval(function(){
+// 		socket.emit("hello",Math.random());
+// 	},1000);
+//
+// });
+//
+// var
+// 	http = require("http"),
+// 	httpserv = http.createServer(handleHTTP),
+//
+// 	port = 3000,
+// 	host = "127.0.0.1",
+//
+// 	ASQ = require("asynquence"),
+// 	node_static = require("node-static"),
+// 	io = require("socket.io").listen(httpserv)
+// ;
+//
+// require("asynquence-contrib");
+//
+// // configure socket.io
+// io.on("configure",function(){
+// 	io.enable("browser client minification"); // send minified client
+// 	io.enable("browser client etag"); // apply etag caching logic based on version number
+// 	io.set("log level", 1); // reduce logging
+// 	io.set("transports", [
+// 		"websocket",
+// 		"xhr-polling",
+// 		"jsonp-polling"
+// 	]);
+// });
+//
+// httpserv.listen(port, host);
